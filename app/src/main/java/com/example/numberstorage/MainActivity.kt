ee52a5e6da32e121cf168bacaf1ba554e7e847c3
gretakasongo <gretakasongo07@gmail.com>
2026-05-22 10:43:51 +0200

    variable for array

---
+++ app/src/main/java/com/example/numberstorage/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.numberstorage
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Array of size 10
+        var numbers = IntArray(10)
+        
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val editTextNumber = findViewById<EditText>(R.id.editTextNumber)
+        val buttonSave =  findViewById<Button>(R.id.buttonSave)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
